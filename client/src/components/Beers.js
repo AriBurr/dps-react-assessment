0a22586657e7a44c8178fdca462038853d0bbcd9
Ariel Burriss <ariburr@gmail.com>
2017-12-26 15:20:33 -0700

    add infinite scroll to beers

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
 import { connect } from 'react-redux';
 import { setFlash } from '../actions/flash';
-import { Card, Image, Dimmer, Loader, Segment } from 'semantic-ui-react';
+import { Card, Container, Header, Image, Dimmer, Loader, Segment } from 'semantic-ui-react';
 
 class Beers extends React.Component {
-  state = { beers: [], loading: true }
+  state = { beers: [], loading: true, page: 1, hasMore: true }
 
-  componentDidMount() {
+  componentWillMount() {
+    this.fetchBeers(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ beers: [], loading: true, hasMore: true, page: 1 });
+    this.fetchBeers(nextProps, 1);
+  }
+
+  fetchBeers = (props, page = 1) => {
     const { dispatch } = this.props;
-    axios.get('/api/all_beers')
-      .then( res => {
-        this.setState({ beers: res.data.entries, loading: false})
-      })
-      .catch( err => {
-        dispatch(setFlash('Unable to retrieve beers. Please try again', 'red'))
-      });
+    axios.get(`/api/all_beers?page=${page}&per_page=30`)
+    .then( res => {
+      const { data } = res;
+      if(data.total_pages) {
+        if (data.total_pages === page)
+          this.setState({ hasMore: false});
+        this.setState({ beers: [...this.state.beers, ...data.entries], total_pages: data.total_pages, page })
+      } else {
+        this.setState({ beers: data.entries, hasMore: false })
+      }
+    })
+    .catch( err => {
+      dispatch(setFlash('Unable to retrieve beers. Please try again', 'red'))
+    })
+    .then( () => {
+      this.setState({ loading: false });
+    });
   }
 
   loadingMessage = () => {
     return (
-      <Dimmer active>
+      <Dimmer active style={{ height: '100vh' }}>
         <Loader>Loading</Loader>
       </Dimmer>
     );
+  }
+
+  loadMoreBeers = () => {
+    this.fetchBeers(this.props, this.state.page + 1)
   }
 
   displayBeers = () => {
@@ -42,11 +66,32 @@ class Beers extends React.Component {
   }
 
   render() {
-    return (
-    <Card.Group stackable itemsPerRow={3}>
-      { this.state.loading ? this.loadingMessage() : this.displayBeers() }
-    </Card.Group>
-    );
+    const { page, hasMore, loading } = this.state;
+    if (loading) {
+      return (
+        <Container>
+          { this.loadingMessage() }
+        </Container>
+      )
+    } else {
+      return (
+        <Container>
+          <Header as='h1' textAlign='center' block>Beers</Header>
+          <Segment style={{ height: '100vh', overflowY: 'scroll', overflowX: 'hidden' }}>
+          <InfiniteScroll
+            pageStart={page}
+            loadMore={this.loadMoreBeers}
+            hasMore={hasMore}
+            useWindow={false}
+          >
+            <Card.Group stackable itemsPerRow={3}>
+              { this.displayBeers() }
+            </Card.Group>
+          </InfiniteScroll>
+        </Segment>
+        </Container>
+      );
+    }
   }
 
 }
--- client/src/components/Breweries.js
+++ client/src/components/Breweries.js
@@ -2,14 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { setFlash } from '../actions/flash';
-import { Card, Image, Dimmer, Loader, Segment } from 'semantic-ui-react';
+import { Card, Container, Header, Image, Dimmer, Loader, Segment } from 'semantic-ui-react';
 
 class Breweries extends React.Component {
   state = { breweries: [], loading: true }
 
   componentDidMount() {
     const { dispatch } = this.props;
-    axios.get('/api/all_breweries')
+    axios.get('/api/all_breweries?page=1&per_page=10')
       .then( res => {
         this.setState({ breweries: res.data.entries, loading: false})
       })
@@ -43,9 +43,12 @@ class Breweries extends React.Component {
 
   render() {
     return (
-    <Card.Group stackable itemsPerRow={3}>
-      { this.state.loading ? this.loadingMessage() : this.displayBreweries() }
-    </Card.Group>
+      <Container>
+        <Header as='h1' textAlign='center block'>Beers</Header>
+        <Card.Group stackable itemsPerRow={3}>
+          { this.state.loading ? this.loadingMessage() : this.displayBreweries() }
+        </Card.Group>
+      </Container>
     );
   }
 
